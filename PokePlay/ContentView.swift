11870d9b5cb17b3537571f5404a08a92dbbd1e64
David Paiva <david.paiva+github@my.istec.pt>
2023-06-07 22:00:57 +0100

    Initial Commit

---
+++ PokePlay/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PokePlay
+//
+//  Created by David Paiva on 07/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokePlay/PokePlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokePlayApp.swift
+//  PokePlay
+//
+//  Created by David Paiva on 07/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct PokePlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
