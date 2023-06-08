32b1b7336d7d08a40fba995844b471b4b07ea100
David Paiva <david.paiva+github@my.istec.pt>
2023-06-08 16:42:48 +0100

    Implement Select Starter Screen

--- PokePlay/SelectStarterScreen.swift
+++ PokePlay/SelectStarterScreen.swift
@@ -6,13 +6,14 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct SelectStarterScreen: View {
     @AppStorage("starter") private var starter = 0
+    @State private var goingToNextScreen = false
     
-    func getPath() -> Void {
-        let poke = PokeUtils.GetPokemonById(id: 1)
-        print(poke)
+    func getImageUrl(id: Int) -> URL? {
+        return URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(id).png") ?? nil
     }
     
     var body: some View {
@@ -21,7 +22,85 @@ struct SelectStarterScreen: View {
                 Text("Select your starter")
                     .font(.title)
                     .fontWeight(.bold)
-                    .onAppear(perform: getPath)
+                    .padding(.bottom, 200)
+                
+                HStack() {
+                    VStack() {
+                        WebImage(url: getImageUrl(id: 1))
+                            .frame(width: 90, height: 50)
+                            .padding()
+                        Text("Bulbasaur")
+                            .font(.title3)
+                            .padding(.bottom, 5)
+                    }
+                    .foregroundColor(.white)
+                    .background(Color.green)
+                    .cornerRadius(20)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(.black, lineWidth: starter == 1 ? 5 : 0)
+                    )
+                    .frame(width: 120, height: 100)
+                    .aspectRatio(contentMode: .fit)
+                    .onTapGesture {
+                        starter = 1
+                    }
+                    
+                    VStack() {
+                        WebImage(url: getImageUrl(id: 4))
+                            .frame(width: 90, height: 50)
+                            .padding()
+                        Text("Charmander")
+                            .font(.title3)
+                            .padding(.bottom, 5)
+                    }
+                    .foregroundColor(.white)
+                    .background(Color.red)
+                    .cornerRadius(20)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(.black, lineWidth: starter == 4 ? 5 : 0)
+                    )
+                    .frame(width: 120, height: 100)
+                    .aspectRatio(contentMode: .fit)
+                    .onTapGesture {
+                        starter = 4
+                    }
+                    
+                    VStack() {
+                        WebImage(url: getImageUrl(id: 7))
+                            .frame(width: 90, height: 50)
+                            .padding()
+                        Text("Squirtle")
+                            .font(.title3)
+                            .padding(.bottom, 5)
+                    }
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(20)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(.black, lineWidth: starter == 7 ? 5 : 0)
+                    )
+                    .frame(width: 120, height: 100)
+                    .aspectRatio(contentMode: .fit)
+                    .onTapGesture {
+                        starter = 7
+                    }
+                    
+                }
+                .padding()
+                
+                Button("Start Your Adventure!", action: {
+                    if (starter == 0) { }
+                    else {
+                        self.goingToNextScreen = true
+                    }
+                })
+                .buttonStyle(.borderedProminent)
+                .disabled(starter == 0)
+                .padding(.top, 200)
+                
             }
             .padding()
         }
